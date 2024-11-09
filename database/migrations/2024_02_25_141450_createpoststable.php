2609d680fae3767c891033dfc6601f1b7459eff6
Masirika Constantin <elconstant73@gmail.com>
2024-11-09 19:19:41 +0100

    Suite 2.2

--- database/migrations/2024_02_25_141450_createpoststable.php
+++ database/migrations/2024_02_25_141450_createpoststable.php
@@ -14,6 +14,12 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('titre');
+            $table->timestamps();
+        });
+        
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('titre');
