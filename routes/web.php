5c96ae4177cdc503a6da5671857aaf1e7c148aad
Masirika Constantin <elconstant73@gmail.com>
2024-11-10 15:47:14 +0100

    Astuces 1

--- routes/web.php
+++ routes/web.php
@@ -144,7 +144,7 @@ Route::get('/user/contact', [UserControl::class])->name('contact1');
 Route::post('/user/{post}/contact', [UserControl::class,'contact'])->name('user.contact');
 
 
-Route::prefix('astuces')->name('astuces.')->middleware('rolemanager:admin')->controller(AstucesControllers::class)->group(function (){
+Route::prefix('astuces')->name('astuces.')->middleware('rolemanager:utilisateur')->controller(AstucesControllers::class)->group(function (){
 
 
     Route::get('/new','create')->name('new');
