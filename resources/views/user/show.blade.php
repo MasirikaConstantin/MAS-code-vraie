572c572bb91715c23a2993aacf97b9925e73ea8a
Masirika Constantin <elconstant73@gmail.com>
2024-11-10 01:44:09 +0100

    echo Post45d3d

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -375,7 +375,7 @@ setlocale(LC_TIME,'fr_FR.utf8');
                         <button type="button" class="focus:odutline-none"  data-modal-target="userModal{{$count1}}" data-modal-toggle="userModal{{$count1}}">
 
                             <p class="inline-flex items-center mr-3 font-semibold text-sm text-gray-900 adrks:text-white">
-                                <svg class="mr-2 w-6 h-6 text-gray-800 adrks:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <svg class="mr-2 w-8 h-8 text-gray-200 adrks:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18Zm0 0a8.949 8.949 0 0 0 4.951-1.488A3.987 3.987 0 0 0 13 16h-2a3.987 3.987 0 0 0-3.951 3.512A8.948 8.948 0 0 0 12 21Zm3-11a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
                                   </svg>
                                 </p>
