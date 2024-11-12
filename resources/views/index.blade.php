791cebdc031f82b797a62359298c05a14191d176
Masirika Constantin <elconstant73@gmail.com>
2024-11-12 11:08:57 +0100

    Style et formations 1.3

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -345,9 +345,7 @@
         <a type="button" href="{{route('tous',['category_id' => $category->id])}}" title="{{$category->titre}}" data-bs-content="{{$category->description}}" data-bs-placement="top" data-bs-trigger="hover" class="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center adrks:focus:ring-gray-600 adrks:bg-gray-800 adrks:border-gray-700 adrks:text-white adrks:hover:bg-gray-700 me-2 mb-2">
           @if ($category->image)
           <div class="w-6 h-5 me-2 -ms-1">
-            @php
-              echo $category->imageUrlcat()
-            @endphp
+            <img src="{{ $category->imageUrlcat()}}" alt="">
           </div>
           @else
           <div class="w-6 h-5 me-2 -ms-1">
