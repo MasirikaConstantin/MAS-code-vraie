373674f60eb4ba6f62fb52cd7e2191a39ea5d092
Masirika Constantin <elconstant73@gmail.com>
2024-11-14 13:40:48 +0100

    Style et formations 2.2

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,23 +1,23 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-200">
+    <div class="mb-4 text-sm text-gray-300">
         {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
     </div>
 
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('password.email') }}">
+    <form method="POST" action="{{ route('password.email') }}" class="space-y-6">
         @csrf
 
         <!-- Email Address -->
         <div>
-            <label for="email" class="text-gray-100 mb-3"  >{{__('Email')}}</label>
-            <x-text-input id="email" class="block mt-1 w-full text-gray-900" type="email" name="email" :value="old('email')" required autofocus />
+            <label for="email" class="block text-gray-100 mb-1">{{ __('Email') }}</label>
+            <x-text-input id="email" class="block w-full mt-1 text-gray-900" type="email" name="email" :value="old('email')" required autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
+        <div class="flex items-center justify-end">
+            <x-primary-button class="ml-3">
                 {{ __('Email Password Reset Link') }}
             </x-primary-button>
         </div>
