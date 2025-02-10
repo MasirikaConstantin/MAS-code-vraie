49cf3d13a3fef0da253611cdb2286169cedae4a7
Masirika Constantin <elconstant73@gmail.com>
2025-02-10 21:40:19 +0100

    Editor 5

--- resources/views/user/astuces.blade.php
+++ resources/views/user/astuces.blade.php
@@ -1,3 +1,6 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>{{ $astuce->titre }} - Mas Code Product</title>
 <meta name="description" content="{{ $astuce->description }}" />
 <meta name="keywords" content="{{ implode(', ', $astuce->tags->pluck('nom')->toArray()) }}" />
