bba19bdad827ef6d46a9558597711814aadc8bbe
Masirika Constantin <elconstant73@gmail.com>
2025-03-25 20:16:05 +0100

    suiteé1

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -174,11 +174,11 @@
                             <td class="px-6 py-4 whitespace-nowrap">{{ $item->email }}</td>
                             <td class="px-6 py-4 whitespace-nowrap">
                                 @if ($item->image)
-                                    <img class="h-12 w-12 rounded-full object-cover" 
+                                    <img class="h-6 w-6 rounded-full object-cover" 
                                          src="{{ $item->imageUrls() }}" 
                                          alt="{{ $item->name }}">
                                 @else
-                                    <img class="h-12 w-12 rounded-full" 
+                                    <img class="h-6 w-6 rounded-full" 
                                          src="{{ asset('téléchargement.png') }}" 
                                          alt="Default">
                                 @endif
@@ -192,7 +192,7 @@
                                         @method('DELETE')
                                     
                                         <button type="submit" onclick="return confirm('Êtes-vous sûr de vouloir supprimer cet utilisateur {{ $item->name }} ?')" 
-                                            class="px-6 py-3 bg-red-500 hover:bg-red-600 text-white rounded-lg transition-colors duration-300 flex items-center gap-2">
+                                            class="px-4 py-1 bg-red-500 hover:bg-red-600 text-white rounded-lg transition-colors duration-300 flex items-center gap-2">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                         </svg>
