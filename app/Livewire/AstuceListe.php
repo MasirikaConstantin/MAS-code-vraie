9685be4fa9735fe3609974fa0bde107dcc594de6
Masirika Constantin <elconstant73@gmail.com>
2025-03-29 21:27:49 +0100

    live

---
+++ app/Livewire/AstuceListe.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Astuce;
+use App\Models\Categorie;
+use Livewire\Component;
+
+class AstuceListe extends Component
+{
+    public $search = '';
+    public function Recherche(){
+        $client  = Astuce::when($this->search, function($query) {
+            return $query->where('titre', 'like', '%'.$this->search.'%')
+                    ->orWhere('contenus', 'like', '%'.$this->search.'%');
+        })
+        ->paginate(10);
+        return $client;
+    }
+    public function render()
+    {
+        
+        return view('livewire.astuce-liste', [
+            'astuces' =>$this->Recherche(),
+            'categories' => Categorie::select('id','titre','description','couleur','image', 'svg')->get(),
+
+
+        ]);
+    }
+}
